c6dd7bfd0b1803d89dc8cf189865e4a4e58343b5
unknown <mic7x4@gmail.com>
2020-11-24 21:47:39 +0200

    adding user scopes

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,5 +2,13 @@ export const authEndpoint =
 "https://accounts.spotify.com/authorize";
 
 const redirectURL = "http://localhost:3000";
+const clientId = "e0bc3456ba624c7b868bc02a1340185e";
 
+const scopes =[
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
 
