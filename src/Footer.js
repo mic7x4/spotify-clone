c99498d6eabfa457179bceba7e91878b45cb4e98
unknown <mic7x4@gmail.com>
2020-11-25 12:36:03 +0200

    working on the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>I am a footer</h1>
+            <div className="footer__left">
+                <img
+                className="footer__albumLogo"
+                src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Usher_Cannes_2016_retusche.jpg" alt=""/>
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleFilledIcon  fontSize="large" />
+                <SkipNextIcon className ="footer__icon"/>
+                <RepeatIcon className="footer__green"/>
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item x5>
+                        <Slider/>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
